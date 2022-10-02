b7bd16ad6910330cad33705b8c26b87846fb347f
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-10-02 19:51:13 +0300

    Created the base urls files

--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    HttpResponse("this is the home page")
+
+
+def room(request):
+    HttpResponse("this is the room page")
