1259f712e1c0ca1d43d8e9b16c2ff4fd91f8acac
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-10-06 11:38:56 +0300

    Updated the Room class

--- base/models.py
+++ base/models.py
@@ -3,8 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Room(models.Model):
-    pass
-class Host(models.Model):
-    pass
+    #host
     #topic
-    #name
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    #participants=
+    updated= models.DateTimeField(auto_now = True)
+    created= models.DateTimeField(auto_now_add = True)
+
+
+    def __str__(self):
+        return self.name
