f416f9e4afb6680ce84c92a1c2187c25e2367678
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-10-23 22:28:44 +0300

    Updated views.py

--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -22,5 +23,12 @@ def room(request,pk):
     return render(request, 'base/room.html', context)
 
 def createRoom(request):
-    context={}
+    form = RoomForm()
+    if request.method =='POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context={'form':form}
     return render(request,'base/room_form.html', context)
