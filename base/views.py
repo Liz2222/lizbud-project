c297a0da1c84d90b0425ac7cffa7ffade611a3c2
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-12-23 23:30:09 +0300

    added participants field

--- base/views.py
+++ base/views.py
@@ -82,6 +82,7 @@ def logout_user(request):
 def room(request,pk):
     room= Room.objects.get(id=pk)
     room_messages=room.message_set.all().order_by('-created')
+    participants=room.participants.all()
 
     if request.method == 'POST':
         message=Message.objects.create(
@@ -93,7 +94,8 @@ def room(request,pk):
 
     context={
         'room':room,
-        'room_messages':room_messages
+        'room_messages':room_messages,
+        'participants':participants
     }
     return render(request, 'base/room.html', context)
 
